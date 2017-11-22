a2c9e46585da31741d83d8dee53513bf1bfb42e3
Abdul Halim <abdulhalim.cu10@gmail.com>
2017-11-22 15:42:31 +0600

    Signature fields added

---
+++ static/src/js/models.js
@@ -0,0 +1,6 @@
+// {
+//     // Custom
+//     model: 'res.company',
+//     fields: ['currency_id', 'email', 'website', 'company_registry', 'vat', 'name', 'phone', 'partner_id', 'country_id', '']
+//
+// }
